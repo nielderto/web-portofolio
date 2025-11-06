255e694df79407a57646fc439716d27284816685
nielderto <nielderto@gmail.com>
2025-11-06 14:46:42 +0800

    changed skills section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,18 +6,19 @@ export default function Skills() {
       <div className="w-full max-w-2xl">
         <div className="text-left mb-12">
           <h2 className="text-3xl font-bold font-jetbrains-mono text-gray-300 inline-block relative">
-            SKILLS
+            TECH STACK
             <span className="absolute left-0 bottom-0 h-[1px] bg-gray-400 w-[120%]"></span>
           </h2>
         </div>
         <div className="flex flex-wrap gap-2 items-center justify-start">
-          <Badge className="text-sm md:text-base px-3 py-1">HTML</Badge>
-          <Badge className="text-sm md:text-base px-3 py-1">CSS</Badge>
-          <Badge className="text-sm md:text-base px-3 py-1">JavaScript</Badge>
           <Badge className="text-sm md:text-base px-3 py-1">TypeScript</Badge>
           <Badge className="text-sm md:text-base px-3 py-1">React</Badge>
           <Badge className="text-sm md:text-base px-3 py-1">Next.js</Badge>
           <Badge className="text-sm md:text-base px-3 py-1">Tailwind CSS</Badge>
+          <Badge className="text-sm md:text-base px-3 py-1">React Query</Badge>
+          <Badge className="text-sm md:text-base px-3 py-1">Zustand</Badge>
+          <Badge className="text-sm md:text-base px-3 py-1">Shadcn</Badge>
+          <Badge className="text-sm md:text-base px-3 py-1">Supabase</Badge>
           <Badge className="text-sm md:text-base px-3 py-1">Git</Badge>
         </div>
       </div>
